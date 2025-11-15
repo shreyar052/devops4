1921f0759130aaa40ca34736fe00abd4a8729961
Shreya Ravindra <1ds23cb052@dsce.edu.in>
2025-11-15 19:16:20 +0530

    Update and rename helloworld.java to HelloWorld.java

--- src/main/java/com/example/helloworld.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-public class HelloWorld {
-public static void main(String[] args) {
-System.out.println(&quot;Hello, DevOps CI/CD using GitHub Actions!&quot;);
-System.out.println(&quot;Change #1: Verifying automatic workflow trigger on push.&quot;);
-}
-}
